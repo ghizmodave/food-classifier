63a6d20cc1ed7689cee2833d2ff75fa4ca776ace
davidemariani <davidemarianidesign@gmail.com>
2020-03-01 14:18:35 +0000

    model.py modified in order to load model_dict() locally

--- pytorch_source/model.py
+++ pytorch_source/model.py
@@ -1,4 +1,5 @@
+import torch
 import torchvision.models as models
 
 #importing pretrained ResNet for transfer learning
-ResNetTransfer = models.resnet50(pretrained = True)
+ResNetTransfer = models.resnet50().load_state_dict(torch.load(path_model))
