c8c9d66bfbac08f90a77f2a0846752a3b6f1102d
davidemariani <davidemarianidesign@gmail.com>
2020-03-05 17:35:12 +0000

    Model retrieval issue SOLVED : PyTorchModel and RealTimePredictor have been used to correctly call and deploy an existing pytorch model from S3, deploy.py and requirements.txt are also called during the deployment. Still more work needs to be done for the output format - which is of type byte now

--- pytorch_source/model.py
+++ pytorch_source/model.py
@@ -2,4 +2,4 @@ import torch
 import torchvision.models as models
 
 #importing pretrained ResNet for transfer learning
-ResNetTransfer = models.resnet50(pretrained=True) #.load_state_dict(torch.load("resnet50_base.pt"))
+ResNetTransfer = models.resnet50(pretrained=True) 
